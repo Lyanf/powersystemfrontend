2f4e60994b6cc73c627cd6a73856b901e9c68ba5
lyf <609136080@qq.com>
2019-04-23 22:29:48 +0800

    file search can run, by js

--- src/views/FileSearch.vue
+++ src/views/FileSearch.vue
@@ -1,4 +1,3 @@
-
 <template>
   <el-container>
     <el-header>
@@ -12,71 +11,237 @@
         <el-main class="main">
           <div id="input_area">
             <el-row :gutter="10">
-              <el-col :span="5">
-                <el-input
-                  placeholder="时间范围"
-                  suffix-icon="el-icon-date"
-                  v-model="input6">
-                </el-input>
+              <el-col :span="10">
+                <!--<el-input-->
+                <!--placeholder="时间范围"-->
+                <!--suffix-icon="el-icon-date"-->
+                <!--v-model="input6">-->
+                <!--</el-input>-->
+                <el-date-picker
+                  v-model="input1"
+                  type="daterange"
+                  range-separator="至"
+                  start-placeholder="开始日期"
+                  end-placeholder="结束日期">
+                </el-date-picker>
               </el-col>
               <el-col :span="5">
                 <el-input
                   placeholder="上传人"
                   suffix-icon="el-icon-date"
-                  v-model="input7">
+                  v-model="input2">
                 </el-input>
               </el-col>
               <el-col :span="5">
                 <el-input
                   placeholder="文件名"
                   suffix-icon="el-icon-date"
-                  v-model="input8">
+                  v-model="input3">
                 </el-input>
               </el-col>
 
             </el-row>
             <el-row>
-              <el-button>查询</el-button>
+              <el-button v-on:click="searchButton">查询</el-button>
             </el-row>
           </div>
-          <FileTable></FileTable>
+          <div>
+            <el-table
+              :data="tableData"
+              border
+              style="width: 100%">
+              <el-table-column
+                type="index"
+                width="50">
+              </el-table-column>
+              <!-- <el-table-column
+                      prop="date"
+                      label="日期"
+                      width="180">
+                    </el-table-column> -->
+              <el-table-column
+                prop="filename"
+                label="文件名"
+                width="180">
+              </el-table-column>
+              <!-- <el-table-column
+                      prop="address"
+                      label="地址"
+                      width="180">
+                    </el-table-column> -->
+              <el-table-column
+                label="上传人"
+                prop="creator"
+              >
+                <!--<template slot-scope="scope">-->
+                <!--<a :href="'/api/downloadFile/' + scope.row.fileID">-->
+                <!--<el-progress :percentage="abc"></el-progress>-->
+                <!--<span>alvis</span>-->
+
+                <!--</a>-->
+                <!-- <el-button type="text" size="small">编辑</el-button> -->
+                <!--</template>-->
+              </el-table-column>
+              <el-table-column
+                label="上传时间"
+                prop="createtime"
+              >
+                <!--<template slot-scope="scope">-->
+                <!--<a :href="'/api/downloadFile/' + scope.row.fileID">-->
+                <!--<el-progress :percentage="abc"></el-progress>-->
+                <!--<span>20190423</span>-->
+
+                <!--</a>-->
+                <!-- <el-button type="text" size="small">编辑</el-button> -->
+                <!--</template>-->
+              </el-table-column>
+              <el-table-column
+                label="文件大小"
+                prop="size"
+              >
+                <!--<template slot-scope="scope">-->
+                <!--<a :href="'/api/parseFile/' + scope.row.fileID">-->
+                <!--<span>10M</span>-->
+                <!--</a>-->
+                <!--</template>-->
+              </el-table-column>
+              <el-table-column
+                label="文件类型"
+                prop="type"
+              >
+                <!--<template slot-scope="scope">-->
+                <!--<a :href="'/api/parseFile/' + scope.row.fileID">-->
+                <!--<span>源数据</span>-->
+                <!--</a>-->
+                <!--</template>-->
+              </el-table-column>
+              <el-table-column
+                label="操作"
+              >
+                <template slot-scope="scope">
+                  <a :href="'/api/downloadFile/' + scope.row.fileID">
+                    <!--<el-progress :percentage="abc"></el-progress>-->
+                    <span>下载</span>
+
+                  </a>
+                  <!-- <el-button type="text" size="small">编辑</el-button> -->
+                </template>
+              </el-table-column>
+            </el-table>
+          </div>
         </el-main>
       </el-container>
 
     </el-container>
   </el-container>
 </template>
-<script type="text/javascript">
-  import NavMenu from '@/components/NavMenu'
-  import InquireData from '@/components/InquireData'
-  import Sidebar from '@/components/Sidebar'
-  import UploadFile from "../components/UploadFile";
-  import FileTable from "../components/FileTable";
+<script>
+  import * as link from '@/api/link'
+  import axios from "axios";
+  import echarts from "echarts";
+  import NavMenu from "../components/NavMenu";
+  import Sidebar from "../components/Sidebar";
 
   export default {
-    name: 'inquire',
-    components: {FileTable, UploadFile, NavMenu,InquireData,Sidebar},
-    created(){
+    name: 'FileSearch',
+    components: {Sidebar, NavMenu},
+
+    data() {
+
+      return {
+        input1: '',
+        input2: '',
+        input3: '',
+        fileList: [],
+        tableData: [],
+        originTableData: [],
+      }
+    },
+    created() {
+      this.fetchData();
+    },
+
+    methods: {
+
+      fetchData() {
+        let that = this
+        link.getFileList.r().then((res) => {
+          that.tableData = res.data
+          this.originTableData = this.tableData
+
+        });
+      },
+      searchButton() {
+        var startDate = '';
+        var endDate = '';
+        try {
+          var startDay = addZero(this.input1[0].getDay());
+          var startYear = (this.input1[0].getFullYear());
+          var startMonth = addZero(parseInt(this.input1[0].getMonth()) + 1).toString();
+
+          var endDay = addZero(this.input1[1].getDay());
+          var endYear = (this.input1[1].getFullYear());
+          var endMonth = addZero(parseInt(this.input1[1].getMonth()) + 1).toString();
+
+          startDate = startYear + "-" + startMonth + "-" + startDay;
+          endDate = endYear + "-" + endMonth + "-" + endDay;
+        } catch (e) {
+
+        }
+        var creatorInput = this.input2;
+        var fileNameInput = this.input3;
+        console.log('123123123')
+        console.log(this.originTableData)
+        var newTableData = []
+        for (var obj of this.originTableData) {
+          if (obj['filename'].includes(fileNameInput)) {
+            if (obj['creator'].includes(creatorInput)) {
+              if (startDate === "" || endDate === "") {
+                newTableData.push(obj)
+              }
+              else{
+                if(startDate<=obj['createtime']&&endDate>=obj['createtime']){
+                  newTableData.push(obj)
+                }
+              }
+            }
+          }
+          console.log('input'+fileNameInput)
+          console.log('fileName'+obj['filename'])
+           }
+        this.tableData = newTableData
+      }
+
 
     }
   }
+
+  function addZero(str) {
+    str = '00' + str;
+    return str.substring(str.length - 2, str.length);
+  }
+
 </script>
 <style type="text/css" scoped>
-  .el-header{
+  .el-header {
     padding: 0px;
     margin-bottom: 10px;
   }
-  .main{
+
+  .main {
     padding: 0px;
     margin-left: 10px;
   }
+
   #input_area {
     margin-top: 10px;
     overflow: hidden;
   }
+
   #result_area {
     margin-top: 10px;
   }
+
   .el-row {
     margin-bottom: 10px;
   }
