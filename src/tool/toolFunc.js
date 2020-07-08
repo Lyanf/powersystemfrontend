4129b567b7179691aeb94f06a866527330d301a7
lyf <609136080@qq.com>
2020-07-08 11:55:31 +0800

    add some units

--- src/tool/toolFunc.js
+++ src/tool/toolFunc.js
@@ -1,16 +1,16 @@
 import moment from "moment";
 function getUnit(originSelect) {
-  let dict = {'FKA': 'kWh', 'BKA': 'kWh', 'FKR': 'kVarh', 'BKR': 'kVarh', 'FFR': '吨', 'BFR': '吨', 'FRH': '立方米', 'GAS': '立方米', 'Water': '吨', 'FRL': '立方米', 'FKA1': 'kWh', 'FKA2': 'kWh', 'FKA3': 'kWh', 'PW': 'kWh', 'AA': 'A', 'BA': 'A', 'CA': 'A', 'AV': 'V', 'BV': 'V', 'CV': 'V', 'ABV': 'V', 'BCV': 'V', 'CAV': 'V', 'IFR': '立方米', 'IFRA': 'A', 'FBFR': '立方米'}
+  let dict = {'MD':'kW','Fee1':'元','FKA': 'kWh', 'BKA': 'kWh', 'FKR': 'kVarh', 'BKR': 'kVarh', 'FFR': '吨', 'BFR': '吨', 'FRH': '立方米', 'GAS': '立方米', 'Water': '吨', 'FRL': '立方米', 'FKA1': 'kWh', 'FKA2': 'kWh', 'FKA3': 'kWh', 'PW': 'kWh', 'AA': 'A', 'BA': 'A', 'CA': 'A', 'AV': 'V', 'BV': 'V', 'CV': 'V', 'ABV': 'V', 'BCV': 'V', 'CAV': 'V', 'IFR': '立方米', 'IFRA': 'A', 'FBFR': '立方米'}
 
   return dict[originSelect]
 }
 function getAllMeasurePointByDict() {
-  let dict = {'FKA': 'kWh', 'BKA': 'kWh', 'FKR': 'kVarh', 'BKR': 'kVarh', 'FFR': '吨', 'BFR': '吨', 'FRH': '立方米', 'GAS': '立方米', 'Water': '吨', 'FRL': '立方米', 'FKA1': 'kWh', 'FKA2': 'kWh', 'FKA3': 'kWh', 'PW': 'kWh', 'AA': 'A', 'BA': 'A', 'CA': 'A', 'AV': 'V', 'BV': 'V', 'CV': 'V', 'ABV': 'V', 'BCV': 'V', 'CAV': 'V', 'IFR': '立方米', 'IFRA': 'A', 'FBFR': '立方米'}
+  let dict = {'MD':'kW','Fee1':'元','FKA': 'kWh', 'BKA': 'kWh', 'FKR': 'kVarh', 'BKR': 'kVarh', 'FFR': '吨', 'BFR': '吨', 'FRH': '立方米', 'GAS': '立方米', 'Water': '吨', 'FRL': '立方米', 'FKA1': 'kWh', 'FKA2': 'kWh', 'FKA3': 'kWh', 'PW': 'kWh', 'AA': 'A', 'BA': 'A', 'CA': 'A', 'AV': 'V', 'BV': 'V', 'CV': 'V', 'ABV': 'V', 'BCV': 'V', 'CAV': 'V', 'IFR': '立方米', 'IFRA': 'A', 'FBFR': '立方米'}
 
   return Object.keys(dict)
 }
 function getMeasurePointChineseName(measurePointEnglishName) {
-  let dict =  {'FKA': '正向有功电度', 'BKA': '反向有功电度', 'FKR': '正向无功电度', 'BKR': '反向无功电度', 'FFR': '正向累积流量', 'BFR': '反向累积流量', 'FRH': '流量计累积流量高位', 'GAS': '用汽量', 'Water': '用水量', 'FRL': '流量计累积流量低位', 'FKA1': '正向有功电度1', 'FKA2': '正向有功电度2', 'FKA3': '正向有功电度3', 'PW': '用电量', 'AA': 'A相电流', 'BA': 'B相电流', 'CA': 'C相电流', 'AV': 'A相电压', 'BV': 'B相电压', 'CV': 'C相电压', 'ABV': 'AB线电压', 'BCV': 'BC线电压', 'CAV': 'CA线电压', 'IFR': '瞬时流量', 'IFRA': '瞬时流量对应电流值', 'FBFR': '正反向累积流量'}
+  let dict =  {'MD':'负荷最大值','Fee1':'电费','FKA': '正向有功电度', 'BKA': '反向有功电度', 'FKR': '正向无功电度', 'BKR': '反向无功电度', 'FFR': '正向累积流量', 'BFR': '反向累积流量', 'FRH': '流量计累积流量高位', 'GAS': '用汽量', 'Water': '用水量', 'FRL': '流量计累积流量低位', 'FKA1': '正向有功电度1', 'FKA2': '正向有功电度2', 'FKA3': '正向有功电度3', 'PW': '用电量', 'AA': 'A相电流', 'BA': 'B相电流', 'CA': 'C相电流', 'AV': 'A相电压', 'BV': 'B相电压', 'CV': 'C相电压', 'ABV': 'AB线电压', 'BCV': 'BC线电压', 'CAV': 'CA线电压', 'IFR': '瞬时流量', 'IFRA': '瞬时流量对应电流值', 'FBFR': '正反向累积流量'}
   return dict[measurePointEnglishName]
 }
 function getMeasurePointAndUnit(measurePoint,hasValue){
